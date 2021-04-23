e7c4314f437834f5dc4564cccf19d16ce5d5ad8b
priyanshiajmera <priyanshiajmera001@gmil.com>
2021-04-23 10:11:24 +0000

    testing

--- webserver.sh
+++ webserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install httpd
+yum install -y httpd
 echo "<h1>this webpage has been buit with jenkins</h1>" > /var/www/html/index.html
 systemctl start httpd
 systemctl status httpd
